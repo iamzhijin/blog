ef1f06f87dc5a0f5b8909a1d47c0e82f5020094b
lzhijin <lzhijin@linewell.com>
2019-10-23 18:21:35 +0800

    前端修改

--- front/blog/src/components/user/Login.vue
+++ front/blog/src/components/user/Login.vue
@@ -1,15 +1,29 @@
 <template>
-    <div class="row">
-        test
-    </div>
+    <div class="container">
+        <div class="col-md-4 col-md-offset-8 content">
+            tt
+        </div>  
+    </div>    
 </template>
 
 <script>
     export default {
-        name: "login"
+        name: "Login"
     }
 </script>
 
 <style scoped>
+html, body{
+    width: 100%;
+    height: 100%;
+}
+    .content {
+        position: relative;
+        top: 50%;
+        height: 300px;
+        background-color: bisque;
+    }
+
+
 
 </style>
